10cb62dc0838ba43c7464dd651bf864920d06cbb
Leonardo <leonardo.appio@gmail.com>
2021-09-21 16:37:49 -0300

    adicionado reducer das acoes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CounterContextProvider } from './contexts/CounterContext';
 import './styles/index.css';
 import Home from './templates/Home';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <CounterContextProvider>
+            <Home />
+        </CounterContextProvider>
+
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
